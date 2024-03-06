a2757a5456cd77a174009d5a5a53a1551978b3e1
Your Username <amyne.boutallagh@gmail.com>
2024-03-06 06:52:36 -0800

    Done

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit
+"""
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """gets number of subscribers for a subreddit"""
+
+url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+response = requests.get(url, headers={"User-Agent": "Python-program"},
+                        allow_redirects=False)
+
+if response.status_code >= 300:
+    return 0
+
+data = response.json().get("data")
+return data.get("subscribers")
