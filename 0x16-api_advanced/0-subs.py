8f01a3b58a6df11ba5d1691d7db8c6ab6d210cd6
Your Username <amyne.boutallagh@gmail.com>
2024-03-06 06:59:50 -0800

    Done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,12 +10,12 @@ import requests
 def number_of_subscribers(subreddit):
     """gets number of subscribers for a subreddit"""
 
-url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-response = requests.get(url, headers={"User-Agent": "Python-program"},
-                        allow_redirects=False)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, headers={"User-Agent": "Python-program"},
+                            allow_redirects=False)
 
-if response.status_code >= 300:
-    return 0
+    if response.status_code >= 300:
+         return 0
 
-data = response.json().get("data")
+    data = response.json().get("data")
     return data.get("subscribers")
