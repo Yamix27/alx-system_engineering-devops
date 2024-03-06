2a3576282350962973c7a68969aaed473cd79538
Your Username <amyne.boutallagh@gmail.com>
2024-03-06 14:52:32 -0800

    Done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
-"""queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit
 """
-
-
+0. How many subs?
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """gets number of subscribers for a subreddit"""
-
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, headers={"User-Agent": "Python-program"},
-                            allow_redirects=False)
-
-    if response.status_code >= 300:
-         return 0
-
-    data = response.json().get("data")
-    return data.get("subscribers")
+    """
+    return the number of subscribers for a given subreddit
+    """
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Python/1.0(Alx School 0x16 task 0)'}
+    response = requests.get(url, headers=headers)
+    if (not response.ok):
+        return 0
+    subscriber_count = response.json().get('data').get('subscribers')
+    return subscriber_count
